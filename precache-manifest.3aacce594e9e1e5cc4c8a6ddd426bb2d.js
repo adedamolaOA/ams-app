7feb62a460b7b4357092760c9737972b56a18d8e
Adedamola <adeleke.adedamola@gmail.com>
2019-07-28 15:10:59 -0600

    Updates

---
+++ precache-manifest.3aacce594e9e1e5cc4c8a6ddd426bb2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a734f99f153c342a90e419e96b655a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a097f9651c299ba90ce",
+    "url": "/static/css/main.4a259f0b.chunk.css"
+  },
+  {
+    "revision": "a5ab86181c15010eb39b",
+    "url": "/static/js/2.f74c7e3f.chunk.js"
+  },
+  {
+    "revision": "2a097f9651c299ba90ce",
+    "url": "/static/js/main.466f831b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
